564af4f8f28f7b4715395790dfc022ad454965b2
sukam09 <sukam09@naver.com>
2021-12-29 10:58:42 +0900

    백준 3085, 2294

---
+++ 백준/2294.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+coins = set([int(input()) for _ in range(n)])
+dp = [-1] * (k + 1)
+dp[0] = 0
+
+for i in range(1, k + 1):
+    cand = [dp[i - coin] for coin in coins if i >= coin if dp[i - coin] != -1]
+    if cand:
+        dp[i] = min(cand) + 1
+
+print(dp[k])
