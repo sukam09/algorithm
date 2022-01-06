53058ad34864e7451e04f8ad735b7f7528325455
sukam09 <sukam09@naver.com>
2022-01-06 16:58:06 +0900

    Remove Good Bye, BOJ 2021 directory

--- 백준/Good Bye, BOJ 2021/24040.py
+++
@@ -1,4 +0,0 @@
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    print("TAK") if n % 3 == 2 or n % 9 == 0 else print("NIE")
---
+++ 백준/코딩테스트 시작을 위한 백준 문제 추천/1697.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+n, k = map(int, input().split())
+vis = [0] * 100005
+oob = lambda x: x < 0 or x > 100000
+que = deque([(n, 0)])
+vis[n] = 1
+
+while que:
+    cur, ans = que.popleft()
+    if cur == k:
+        break
+    for nxt in cur - 1, cur + 1, cur * 2:
+        if not oob(nxt) and not vis[nxt]:
+            que.append((nxt, ans + 1))
+            vis[nxt] = 1
+
+print(ans)
