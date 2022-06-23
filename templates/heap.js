765bbba0324a401002ac643b664bd43ea7338d0b
sukam09 <sukam09@naver.com>
2022-06-23 10:45:36 +0900

    refactor: edit heap.js

--- templates/heap.js
+++ templates/heap.js
@@ -54,4 +54,15 @@ class Heap {
   }
 }
 
-export default Heap;
+const heap = new Heap(5);
+
+for (let i = 5; i >= 1; i--) {
+  heap.push(i);
+  console.log(heap);
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(heap.top());
+  heap.pop();
+  console.log(heap);
+}
