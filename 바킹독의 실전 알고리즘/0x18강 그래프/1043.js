47f5868043678cb000fd1e3796fd0357cf184784
sukam09 <sukam09@naver.com>
2022-07-05 14:03:09 +0900

    feat: add 1043.js and 2617.js

---
+++ 바킹독의 실전 알고리즘/0x18강 그래프/1043.js
@@ -0,0 +1,89 @@
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.head = 0;
+    this.tail = 0;
+  }
+
+  push(item) {
+    this.queue[this.tail++] = item;
+  }
+
+  pop() {
+    return this.queue[this.head++];
+  }
+
+  isEmpty() {
+    return this.tail - this.head === 0;
+  }
+}
+
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+const [n, m] = input[0].split(' ').map(Number);
+
+const graph = [];
+for (let i = 1; i <= n; i++) {
+  graph[i] = [];
+}
+
+let start;
+
+for (let i = 1; i < input.length; i++) {
+  const [p, ...pp] = input[i].split(' ').map(Number);
+  if (p === 0) {
+    console.log(m);
+    process.exit();
+  }
+  if (i === 1) {
+    start = pp[0];
+  }
+  if (p === 1) {
+    continue;
+  }
+  for (let j = 0; j < p - 1; j++) {
+    const [a, b] = [pp[j], pp[j + 1]];
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+}
+
+const queue = new Queue();
+const visited = Array(n + 1).fill(false);
+queue.push(start);
+visited[start] = true;
+while (!queue.isEmpty()) {
+  const cur = queue.pop();
+  for (const next of graph[cur]) {
+    if (visited[next]) {
+      continue;
+    }
+    visited[next] = true;
+    queue.push(next);
+  }
+}
+
+const result = visited
+  .map((item, index) => (item ? index : -1))
+  .filter((item) => item !== -1);
+const target = new Set(result);
+let answer = 0;
+
+for (let i = 2; i < input.length; i++) {
+  const line = input[i].split(' ').map(Number);
+  let inflated = true;
+  for (let j = 1; j < line.length; j++) {
+    if (target.has(line[j])) {
+      inflated = false;
+      break;
+    }
+  }
+  if (inflated) {
+    answer++;
+  }
+}
+
+console.log(answer);
---
+++ 바킹독의 실전 알고리즘/0x18강 그래프/2617.js
@@ -0,0 +1,71 @@
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.head = 0;
+    this.tail = 0;
+  }
+
+  push(item) {
+    this.queue[this.tail++] = item;
+  }
+
+  pop() {
+    return this.queue[this.head++];
+  }
+
+  isEmpty() {
+    return this.tail - this.head === 0;
+  }
+}
+
+const bfs = (start, nodes) => {
+  const queue = new Queue();
+  const visited = Array(n + 1).fill(false);
+  queue.push(start);
+  visited[start] = true;
+  let nodeNum = 0;
+
+  while (!queue.isEmpty()) {
+    const cur = queue.pop();
+    for (const next of nodes[cur]) {
+      if (visited[next]) {
+        continue;
+      }
+      visited[next] = true;
+      nodeNum++;
+      queue.push(next);
+    }
+  }
+  return nodeNum;
+};
+
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+const [n, m] = input[0].split(' ').map(Number);
+const parentNodes = [];
+const childNodes = [];
+for (let i = 1; i <= n; i++) {
+  parentNodes[i] = [];
+  childNodes[i] = [];
+}
+
+for (let i = 1; i <= m; i++) {
+  const [parent, child] = input[i].split(' ').map(Number);
+  parentNodes[child].push(parent);
+  childNodes[parent].push(child);
+}
+
+const target = (n + 1) / 2;
+let answer = 0;
+
+for (let i = 1; i <= n; i++) {
+  const [parentNum, childNum] = [bfs(i, parentNodes), bfs(i, childNodes)];
+  if (parentNum >= target || childNum >= target) {
+    answer++;
+  }
+}
+
+console.log(answer);
