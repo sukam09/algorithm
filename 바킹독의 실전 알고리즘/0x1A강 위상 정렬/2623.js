caadb3c4e8067d93fe6357a35d45be553fe923dd
sukam09 <sukam09@naver.com>
2022-08-11 11:16:57 +0900

    ✨ feat: 2623.js 추가

---
+++ 바킹독의 실전 알고리즘/0x1A강 위상 정렬/2623.js
@@ -0,0 +1,60 @@
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.head = 0;
+    this.tail = 0;
+  }
+
+  push(item) {
+    this.queue[this.tail++] = item;
+  }
+
+  pop() {
+    return this.queue[this.head++];
+  }
+
+  isEmpty() {
+    return this.tail - this.head === 0;
+  }
+}
+
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((x) => x.split(" ").map((x) => +x));
+
+const n = input[0][0];
+const indegrees = Array(n + 1).fill(0);
+const graph = Array(n + 1);
+for (let i = 1; i <= n; i++) {
+  graph[i] = [];
+}
+
+for (let i = 1; i < input.length; i++) {
+  for (let j = 1; j < input[i].length - 1; j++) {
+    const [a, b] = [input[i][j], input[i][j + 1]];
+    graph[a].push(b);
+    indegrees[b]++;
+  }
+}
+
+const queue = new Queue();
+for (let i = 1; i <= n; i++) {
+  if (indegrees[i] === 0) queue.push(i);
+}
+
+let answer = "";
+let count = 0;
+while (!queue.isEmpty()) {
+  const cur = queue.pop();
+  answer += `${cur}\n`;
+  count++;
+  for (const next of graph[cur]) {
+    indegrees[next]--;
+    if (indegrees[next] === 0) queue.push(next);
+  }
+}
+
+console.log(count === n ? answer : 0);
