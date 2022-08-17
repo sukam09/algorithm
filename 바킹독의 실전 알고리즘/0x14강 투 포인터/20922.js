51f79648c2c515b02adedce3065fa007c373f279
sukam09 <sukam09@naver.com>
2022-08-17 14:42:30 +0900

    ✨ feat: 20922.js 추가

---
+++ 바킹독의 실전 알고리즘/0x14강 투 포인터/20922.js
@@ -0,0 +1,29 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((x) => x.split(" ").map((x) => +x));
+
+const [n, k] = [input[0][0], input[0][1]];
+const arr = input[1];
+let answer = 0;
+let end = 0;
+const counter = Array(100001).fill(0);
+counter[arr[0]] = 1;
+
+for (let start = 0; start < n; start++) {
+  while (end < n && counter[arr[end + 1]] < k) {
+    end++;
+    if (end < n) {
+      counter[arr[end]]++;
+    }
+  }
+  if (end === n) {
+    break;
+  }
+  answer = Math.max(answer, end - start + 1);
+  counter[arr[start]]--;
+}
+
+console.log(answer);
