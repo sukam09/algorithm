b6d5f14e0240e1c70c01b2519c378f4e4aaf40dc
sukam09 <sukam09@naver.com>
2022-08-23 14:47:21 +0900

    ♻️ refactor: pop 함수를 힙에서 빼는 부분과 맨 위를 확인하는 부분으로 분리 및 MaxHeap 추가

--- templates/Heap.js
+++ templates/Heap.js
@@ -1,15 +1,15 @@
 class Heap {
   constructor() {
-    this.heap = [];
+    this.data = [];
     this.size = 0;
   }
 
   push(item) {
-    this.heap[++this.size] = item;
+    this.data[++this.size] = item;
     let index = this.size;
     while (index > 1) {
       const parentIndex = Math.floor(index / 2);
-      if (this.heap[parentIndex] <= this.heap[index]) {
+      if (this.data[parentIndex] <= this.data[index]) {
         break;
       }
       this.swap(parentIndex, index);
@@ -17,9 +17,12 @@ class Heap {
     }
   }
 
+  top() {
+    return this.data[1];
+  }
+
   pop() {
-    const item = this.heap[1];
-    this.heap[1] = this.heap[this.size--];
+    this.data[1] = this.data[this.size--];
     let index = 1;
     while (2 * index <= this.size) {
       const leftChild = 2 * index;
@@ -27,21 +30,73 @@ class Heap {
       let minChild = leftChild;
       if (
         rightChild <= this.size &&
-        this.heap[rightChild] < this.heap[leftChild]
+        this.data[rightChild] < this.data[leftChild]
       ) {
         minChild = rightChild;
       }
-      if (this.heap[index] <= this.heap[minChild]) {
+      if (this.data[index] <= this.data[minChild]) {
         break;
       }
       this.swap(index, minChild);
       index = minChild;
     }
-    return item;
   }
 
   swap(a, b) {
-    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+    [this.data[a], this.data[b]] = [this.data[b], this.data[a]];
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+class MaxHeap {
+  constructor() {
+    this.data = [];
+    this.size = 0;
+  }
+
+  push(item) {
+    this.data[++this.size] = item;
+    let index = this.size;
+    while (index > 1) {
+      const parentIndex = Math.floor(index / 2);
+      if (this.data[parentIndex] >= this.data[index]) {
+        break;
+      }
+      this.swap(parentIndex, index);
+      index = parentIndex;
+    }
+  }
+
+  top() {
+    return this.data[1];
+  }
+
+  pop() {
+    this.data[1] = this.data[this.size--];
+    let index = 1;
+    while (2 * index <= this.size) {
+      const leftChild = 2 * index;
+      const rightChild = 2 * index + 1;
+      let maxChild = leftChild;
+      if (
+        rightChild <= this.size &&
+        this.data[rightChild] > this.data[leftChild]
+      ) {
+        maxChild = rightChild;
+      }
+      if (this.data[index] >= this.data[maxChild]) {
+        break;
+      }
+      this.swap(index, maxChild);
+      index = maxChild;
+    }
+  }
+
+  swap(a, b) {
+    [this.data[a], this.data[b]] = [this.data[b], this.data[a]];
   }
 
   isEmpty() {
