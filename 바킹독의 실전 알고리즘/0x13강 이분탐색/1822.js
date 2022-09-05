8e3f14b1e8b9c02b093d98246dc1fa26bc00d17f
sukam09 <sukam09@naver.com>
2022-09-05 11:11:22 +0900

    feat: BOJ 1822번 차집합

---
+++ 바킹독의 실전 알고리즘/0x13강 이분탐색/1822.js
@@ -0,0 +1,23 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((line) => line.split(" ").map(Number));
+
+const arrA = input[1];
+const arrB = input[2];
+const setB = new Set(arrB);
+
+let answer = 0;
+const elements = [];
+for (const a of arrA) {
+  if (!setB.has(a)) {
+    answer++;
+    elements.push(a);
+  }
+}
+elements.sort((a, b) => a - b);
+
+console.log(answer);
+console.log(elements.join(" "));
