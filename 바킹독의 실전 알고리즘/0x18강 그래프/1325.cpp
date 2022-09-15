cae2b12c17c27e2471d4d838cfaf107a29062ed8
sukam09 <sukam09@naver.com>
2022-09-15 15:50:20 +0900

    refactor: 변수명 짧게 변경

--- 바킹독의 실전 알고리즘/0x18강 그래프/1325.cpp
+++ 바킹독의 실전 알고리즘/0x18강 그래프/1325.cpp
@@ -4,26 +4,26 @@ using namespace std;
 int n, m;
 vector<int> graph[10005];
 int hacked[10005];
-queue<int> que;
+queue<int> q;
 
 int bfs(int node) {
-  vector<bool> visited(n + 5);
-  visited[node] = true;
-  que.push(node);
-  int count = 1;
-  while (!que.empty()) {
-    int cur = que.front();
-    que.pop();
-    for (int next : graph[cur]) {
-      if (visited[next]) {
+  vector<bool> vis(n + 5);
+  vis[node] = true;
+  q.push(node);
+  int cnt = 1;
+  while (!q.empty()) {
+    int cur = q.front();
+    q.pop();
+    for (int nxt : graph[cur]) {
+      if (vis[nxt]) {
         continue;
       }
-      visited[next] = true;
-      que.push(next);
-      count++;
+      vis[nxt] = true;
+      q.push(nxt);
+      cnt++;
     }
   }
-  return count;
+  return cnt;
 }
 
 int main(void) {
@@ -36,13 +36,13 @@ int main(void) {
     cin >> a >> b;
     graph[b].push_back(a);
   }
-  int maxval = 0;
+  int mx = 0;
   for (int i = 1; i <= n; i++) {
     hacked[i] = bfs(i);
-    maxval = max(maxval, hacked[i]);
+    mx = max(mx, hacked[i]);
   }
   for (int i = 1; i <= n; i++) {
-    if (hacked[i] == maxval) {
+    if (hacked[i] == mx) {
       cout << i << " ";
     }
   }
