82d14939838498c80f865f51fcc362bf0e793092
sukam09 <sukam09@naver.com>
2022-11-16 17:14:26 +0900

    feat: BOJ 16401번 풀이 추가

---
+++ BOJ 랜덤디펜스/16401.js
@@ -0,0 +1,28 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n");
+const [m, n] = input[0].split(" ").map(Number);
+const l = input[1].split(" ").map(Number);
+let st = 1;
+let en = Math.max(...l);
+
+const solve = (mid) => {
+  let cnt = 0;
+  for (let i = 0; i < n; i++) {
+    cnt += Math.floor(l[i] / mid);
+  }
+  return cnt >= m;
+};
+
+let ans = 0;
+while (st < en) {
+  let mid = Math.floor((st + en + 1) / 2);
+  if (solve(mid)) {
+    st = mid;
+    ans = st;
+  } else {
+    en = mid - 1;
+  }
+}
+console.log(ans);
